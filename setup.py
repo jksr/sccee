f62f6506a05cf0121cc2786e5bd23bf43033cf3d
jksr <jksr.tw@gmail.com>
2021-09-17 16:24:56 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SCCEE',
+    version='0.0.1',
+    url='https://github.com/jksr/sccee',
+    author='Wei Tian',
+    author_email='jksr.tw@gmail.com',
+    description='Single Cell Condition Effect Evaluation',
+    packages=find_packages(),    
+    #install_requires=['numpy >= 1.11.1', 'matplotlib >= 1.5.1'],
+)
+
